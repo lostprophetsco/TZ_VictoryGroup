3efb88ce8f2c2e6cc40f7e6355651ed412474c17
Vadim <lostprophetsco@gmail.com>
2024-09-15 20:32:13 +0300

    feat: update template

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import customBackgroundInputRange from './helpers/customBackgroundInputRange.js';
+import numberWithSpaces from './helpers/numberWithSpaces.js';
+import inputTextAutoWidth from './helpers/inputTextAutoWidth.js';
+
+customBackgroundInputRange('.range-default');
+numberWithSpaces('.need-js-to-digits-separate');
+inputTextAutoWidth('.calculator__range-input');
