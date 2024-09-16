9dccbc8617c2a55434286be4f1bc94fd94b4fb22
Vadim <lostprophetsco@gmail.com>
2024-09-16 14:45:53 +0300

    feat: sections completion-head && completion-body done

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,53 @@ import customBackgroundInputRange from './helpers/customBackgroundInputRange.js'
 import numberWithSpaces from './helpers/numberWithSpaces.js';
 import inputTextAutoWidth from './helpers/inputTextAutoWidth.js';
 
+import Glide, {
+  Controls,
+  Breakpoints,
+  Swipe,
+  Keyboard,
+} from '@glidejs/glide/dist/glide.modular.esm';
+
 customBackgroundInputRange('.range-default');
 numberWithSpaces('.need-js-to-digits-separate');
 inputTextAutoWidth('.calculator__range-input');
+
+const glideOptions = {
+  type: 'carousel',
+  startAt: 0,
+  perView: 4,
+  gap: 12,
+  keyboard: true,
+  perTouch: 1,
+  breakpoints: {
+    1024: {
+      perView: 2,
+    },
+    768: {
+      perView: 2,
+    },
+    560: {
+      perView: 1,
+    },
+  },
+};
+
+const glideMount = { Controls, Breakpoints, Swipe, Keyboard };
+const glideHeader = document.querySelector('.completion-head__glide');
+const glideBody = document.querySelector('.completion-body__glide');
+
+const firstGlide = new Glide(glideHeader, {
+  ...glideOptions,
+}).mount(glideMount);
+
+const secondGlide = new Glide(glideBody, {
+  ...glideOptions,
+}).mount(glideMount);
+
+function syncGlide(master, slave) {
+  master.on('run', function (e) {
+    slave.go(e.direction);
+  });
+}
+syncGlide(firstGlide, secondGlide);
+syncGlide(secondGlide, firstGlide);
